646e554576ac81afdf4035265dd1e2ca2dd27130
Bappa21ditya <93249869+Bappa21ditya@users.noreply.github.com>
2023-07-06 09:35:16 +0530

    Add files via upload

---
+++ ServiceRegistry/src/test/java/com/lcwd/ServiceRegistry/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.ServiceRegistry.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
