7876e2c106d2d5646bfb7d3d25598c8e2b62dc1b
Bappa21ditya <93249869+Bappa21ditya@users.noreply.github.com>
2023-07-06 09:33:36 +0530

    Add files via upload

---
+++ ApiGateway/src/main/java/com/lcwd/gateway/model/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.lcwd.gateway.model;
+
+import lombok.Data;
+
+import java.util.Collection;
+
+@Data
+public class AuthResponse {
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+
+    private long expireAt;
+
+    private Collection<String> authorities;
+}
